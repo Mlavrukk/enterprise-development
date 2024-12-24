31b80a9a99a004697afc9149b448398229ae44a7
Mlavrukk <margolav27@yandex.ru>
2024-12-24 18:01:17 +0300

    IdApplication

--- UniversityAdmission/UniversityAdmission.Domain/Repositories/ApplicationRepository.cs
+++ UniversityAdmission/UniversityAdmission.Domain/Repositories/ApplicationRepository.cs
@@ -17,7 +17,7 @@ public class ApplicationRepository(UniversityAdmissionContext context) : IReposi
 
     public async Task<List<Application>> GetAll() => await context.Applications.ToListAsync();
 
-    public async Task<Application?> GetById(int id) => await context.Applications.FirstOrDefaultAsync(a => a.i == id);
+    public async Task<Application?> GetById(int id) => await context.Applications.FirstOrDefaultAsync(a => a.IdApplication == id);
 
     public async Task<Application> Post(Application entity)
     {
